9fe05e7b7cc6767a7355e3843ecfa5357f560467
William Lu <rlu1@stuy.edu>
2019-01-06 17:24:03 -0500

    insert complete

--- commons.c
+++ commons.c
@@ -11,11 +11,11 @@ void print_table(struct table *t){
         printf("| ");
         for (int j = 0; j < t->num_columns; j ++)
             if (t->types[j] == INT)
-                printf("%d\t| ", t->al.rows[i].values[j].integer);
+                printf("[%d]\t| ", t->al.rows[i].values[j].integer);
             else if (t->types[j] == DOUBLE)
-                printf("%lf\t| ", t->al.rows[i].values[j].decimal);
+                printf("[%lf]\t| ", t->al.rows[i].values[j].decimal);
             else if (t->types[j] == STRING)
-                printf("%s\t| ", t->al.rows[i].values[j].string);
+                printf("[%s]\t| ", t->al.rows[i].values[j].string);
         printf("\n");
     }
 }
@@ -41,8 +41,10 @@ char *rs(char *str){
     return str;
 }
 
-void create_table(char *str){
-    struct table *t = malloc(sizeof(struct table));
+void create_table(char *str, struct database *db){
+    struct table *t = db->tables + db->num_tables;
+    db->num_tables ++;
+
     construct(&(t->al));
     str = rs(str);
     strcpy(t->name, rs( strsep(&str, "{") ));
@@ -75,45 +77,94 @@ void create_table(char *str){
         }
         t->names[j] = piece[j];
     }
-
     print_table(t);
+    // return t;
 }
 
-void read_table(char *str){
-
-}
-
-void insert(char *str){
+void read_table(char *str, struct database *db){
 
 }
 
-void delete(char *str){
+void insert(char *str, struct database *db){
+    struct table *t = 0;
+    str = rs(str);
+    // printf("%s\n", db->tables[0].name);
+    // print_table(&(db->tables[0]));
+    for (int i = 0; i < db->num_tables; i ++)
+        if ( !strcmp(db->tables[i].name, rs( strsep(&str, "{") )) )
+            t = db->tables + i;
+    if (!t){
+        printf("table does not exist\n");
+        return;
+    }
+    char *data = strsep(&str, "}");
+    char *row, *piece;
+    while (strsep(&data, "(") && data){
+        row = strsep(&data, ")");
+        // printf("[%s]\n", row);
+        if (!row){
+            printf("missing closing '('\n");
+            return;
+        }
+        struct row r;
+        r.values = calloc(t->num_columns, sizeof(union value));
+        // r = malloc(sizeof(struct row));
+        for (int i = 0; i < t->num_columns; i ++){
+            piece = rs( strsep(&row, ",") );
+            // printf("[%s]\n", piece);
+            if (!piece){
+                printf("insufficient number of parameters\n");
+                return;
+            }
+            if (t->types[i] == INT)
+                r.values[i].integer = atoi(piece);
+            else if (t->types[i] == DOUBLE)
+                r.values[i].decimal = atof(piece);
+            else if (t->types[i] == STRING)
+                r.values[i].string = piece;
+        }
+        add_last(&(t->al), r);
+        // printf("[%s]\n", data);
+    }
+}
+
+void delete(char *str, struct database *db){
 
 }
 
-void drop(char *str){
+void drop(char *str, struct database *db){
     str = rs(str);
 }
 
-void execute(char *str){
+void execute(char *str, struct database *db){
     str = rs(str);
     if (!strncmp(str, "create ", 7))
-        create_table(str + 7);
+        create_table(str + 7, db);
     else if (!strncmp(str, "read ", 5))
-        read_table(str + 5);
+        read_table(str + 5, db);
     else if (!strncmp(str, "insert ", 7))
-        insert(str + 7);
+        insert(str + 7, db);
     else if (!strncmp(str, "delete ", 7))
-        delete(str + 7);
+        delete(str + 7, db);
     else if (!strncmp(str, "drop ", 5))
-        drop(str + 5);
+        drop(str + 5, db);
     else
         printf("Invalid Command\n");
 }
 
 int main(){
+    // int shm_id = shmget(KEY, SEG_SIZE, IPC_CREAT | 0644);
+    struct database *db = malloc( sizeof(struct database) );
+    db->tables = calloc(10, sizeof(struct table));
+    db->num_tables = 0;
     char str[] = "   create  oof  {  int   i  ,  double    d   ,  string   s  } ";
-    execute(str);
+    execute(str, db);
+
+    char ins[] = "  insert    oof  {  (  3 , 6.9 , lmao ) (   5, 3.14  , UwU )   }";
+    execute(ins, db);
+    print_table(&(db->tables[0]));
+
+    // print_table(&(db->tables[0]));
 
     return 0;
 }
--- commons.h
+++ commons.h
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+
 #include "al.h"
 
+#define KEY 52013
+
+#define SEG_SIZE 0xffff
 #define BUFFER_SIZE 1024
 #define DATA_SIZE 256
 
@@ -30,18 +37,26 @@ struct table {
     struct array_list al;
 };
 
+struct database {
+    struct table *tables;
+    int num_tables;
+};
+
 void print_table(struct table *t);
 
 void prompt(char display[], char input[]);
 
-void create_table(char *str);
+//removes begining and ending spaces
+char *rs(char *str);
 
-void read_table(char *str);
+void create_table(char *str, struct database *db);
 
-void insert(char *str);
+void read_table(char *str, struct database *db);
 
-void delete(char *str);
+void insert(char *str, struct database *db);
 
-void drop(char *str);
+void delete(char *str, struct database *db);
 
-void execute(char *str);
+void drop(char *str, struct database *db);
+
+void execute(char *str, struct database *db);
