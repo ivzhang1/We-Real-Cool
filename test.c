585d0911adc8a7b74946c5f02da724a4ab07f680
William Lu <rlu1@stuy.edu>
2019-01-10 14:45:43 -0500

    added strsep_str()

--- test.c
+++ test.c
@@ -2,8 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *strsep_str(char **str, char *delim){
+  char *temp = *str;
+  int len = strlen(delim);
+  char *found = 0;
+  int ctr = 0;
+  while (**str){
+    if ( !strncmp(*str, delim, len) ){
+      found = *str;
+      *str += len;
+      break;
+    }
+    (*str) ++;
+    ctr ++;
+  }
+  if (found)
+    for (int i = 0; i < len; i ++){
+      *found = 0;
+      found ++;
+    }
+  return temp;
+}
+
 int main(){
-    char c = "c";
-    printf("%c\n", c);
-    return 0;
+
+  char *str = malloc(100);
+  strcpy(str, "delete oof wher safafa f");
+
+  char *s = strsep_str(&str, "where");
+  printf("[%s]\n", s);
+  printf("[%s]\n", str);
+
+  return 0;
 }
