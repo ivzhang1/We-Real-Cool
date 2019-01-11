ce21d08bc965f7c715c2954a1cda83bffc7abcb0
ivzhang1 <izhang1@stuy.edu>
2019-01-10 23:20:34 -0500

    get registerr() to work

--- login.c
+++ login.c
@@ -2,36 +2,49 @@
 #include<string.h>
 #include <unistd.h>
 
+void login(){
+  char student_name[256];
+  char pass[256];
+  
+  printf("Student Name: ");
+  scanf("%s", student_name);
+  fflush(stdout);
+  
+  printf("Password: ");
+  scanf("%s", pass);
+  fflush(stdout);
+  
+}
+
 int main(){
-  char
-}
-
-int login(){
   char student_name[256];
   char pass[256];
+  char confirm_pass[256];
   
   printf("Student Name: ");
   scanf("%s", student_name);
   fflush(stdout);
-  
+
   printf("Password: ");
   scanf("%s", pass);
   fflush(stdout);
-}
-
-int registerr(){
-  char student_name[256];
-  char pass[256];
   
-  printf("Student Name: ");
-  scanf("%s", student_name);
-  fflush(stdout);
-  
-  printf("Password: ");
-  scanf("%s", pass);
-  fflush(stdout);
-
   printf("Confirm Password: ");
-  scanf("%s", pass);
-  fflush(stdout);  
+  scanf("%s", confirm_pass);
+  fflush(stdout);
+
+  while(strcmp(pass, confirm_pass) != 0){
+    printf("Passwords don't match! Try again!\n");
+
+    printf("Password: ");
+    scanf("%s", pass);
+    fflush(stdout);
+    
+    printf("Confirm Password: ");
+    scanf("%s", confirm_pass);
+    fflush(stdout);
+  }
+
+  return 0;
+  
 }
