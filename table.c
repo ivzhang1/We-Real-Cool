8b661e0874e569605aa3a3ef5636c00f2e09d11d
SimonBerens <sberensnyc@gmail.com>
2019-01-10 23:25:09 -0500

    Implement hash table

--- table.c
+++ table.c
@@ -1,2 +1,19 @@
+#include <string.h>
+#include "table.h"
 
+// djb2 string hash function developed by dan bernstein
+unsigned long hash(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++)) hash = ((hash << 5) + hash) + c;
+    return hash;
+}
+
+void *lookup(struct hash_table *ht, char *str) {
+    unsigned long h = hash((unsigned char *) str);
+    struct bucket b = ht->buckets[h];
+    int i = 0;
+    for (; strcmp((char *) b.values[i], str); i++);
+    return b.values[i];
+}
 
--- table.h
+++ table.h
@@ -6,17 +6,34 @@
 #define INT 0
 #define DOUBLE 1
 #define STRING 2
+#define COLUMN 3
 
 struct column {
     char name[DATA_SIZE];
     unsigned char tags;
     unsigned char type;
-    unsigned char *data;
+    void **data;
 };
 
 struct table {
     char name[DATA_SIZE];
     struct column *columns;
 };
+
+struct bucket {
+    unsigned long hash;
+    void **values;
+    unsigned char type;
+};
+
+struct hash_table {
+    struct bucket *buckets;
+    unsigned long num_buckets;
+};
+
+unsigned long hash(unsigned char *str);
+
+void *lookup(struct hash_table *ht, char *str);
+
 
 #endif //FINAL_TABLE_H
