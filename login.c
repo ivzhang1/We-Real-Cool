e9d2025cbd2e8d4678786d1b6a4b7a3909783ac6
ivzhang1 <izhang1@stuy.edu>
2019-01-11 10:38:48 -0500

    fixed register

--- login.c
+++ login.c
@@ -22,29 +22,34 @@ int main(){
   char confirm_pass[256];
   
   printf("Student Name: ");
-  scanf("%s", student_name);
-  fflush(stdout);
+  fgets(student_name, 256, stdin);
+  //fflush(stdout);
 
   printf("Password: ");
-  scanf("%s", pass);
-  fflush(stdout);
+  fgets(pass, 256, stdin);
+  //fflush(stdout);
   
   printf("Confirm Password: ");
-  scanf("%s", confirm_pass);
-  fflush(stdout);
+  fgets(confirm_pass, 256, stdin);
+  //fflush(stdout);
 
   while(strcmp(pass, confirm_pass) != 0){
     printf("Passwords don't match! Try again!\n");
 
     printf("Password: ");
-    scanf("%s", pass);
-    fflush(stdout);
+    fgets(pass, 256, stdin);
+    //fflush(stdout);
     
     printf("Confirm Password: ");
-    scanf("%s", confirm_pass);
-    fflush(stdout);
+    fgets(confirm_pass, 256, stdin);
+    //fflush(stdout);
   }
+  printf("%ld\n", strlen(student_name));
+  *(student_name + strlen(student_name) - 1) = '\0';
+  *(pass + strlen(pass) - 1) = '\0';
+  *(confirm_pass + strlen(confirm_pass) - 1) = '\0';
 
+  printf("Success: %s, %s, %s\n", student_name, pass, confirm_pass);
   return 0;
   
 }
