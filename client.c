3a6688004ff35faf3cb515f60a1ee620d0d88cc3
SimonBerens <sberensnyc@gmail.com>
2019-01-17 10:26:31 -0500

    Fix imports

--- client.c
+++ client.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "table.h"
 #include "client.h"
 
 int main(int argc, char * argv[]) {
@@ -17,7 +21,7 @@ int main(int argc, char * argv[]) {
 int connect_to_db(char *ip, char *port) {
     struct addrinfo *results, *p;
     get_results(ip, port, &results);
-    int db_sd;
+    int db_sd = -1;
     for (p = results; p != NULL; p = p->ai_next) {
         if ((db_sd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
             continue;
@@ -49,8 +53,11 @@ char *get_query() {
     char *query_buf = calloc(BUFFER_SIZE, sizeof(char));
     prompt("Enter query: ", query_buf);
     // check if loading from file
-    if (!strncmp(query_buf, "source", 6)) {
-        int fd = error_check("loading file", open(&(query_buf[7]), O_RDONLY));
+    char *temp = query_buf;
+    for (; *temp == ' '; temp++); // dont use next word to modify string
+    if (!strncmp(temp, "source", 6)) {
+        char *file = next_word(&temp);
+        int fd = error_check("loading file", open(file, O_RDONLY));
         query_buf[read(fd, query_buf, BUFFER_SIZE)] = '\0';
     }
     return query_buf;
